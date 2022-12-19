d80bca42a103a6fdb2fd2112ec6da58d6a743366
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2022-12-19 19:22:52 +0100

    We kinda need a redo

--- Assets/Scripts/BoardSetup.cs
+++ Assets/Scripts/BoardSetup.cs
@@ -8,6 +8,7 @@ public class BoardSetup : MonoBehaviour
     [Header("Cock")]
     [SerializeField] Camera mainCamera;
     [SerializeField] GameObject card;
+    [SerializeField] GameObject board;
     [SerializeField] int pairDb;   
 
     List<int> indexes;
@@ -22,90 +23,105 @@ public class BoardSetup : MonoBehaviour
     float width;
 
 
+
     //planed game sizes (heightxwidth) 3x2 4x3 4x4 5x4 6x5 8x5 8x6 9x6
     void Start()
     {
-        switch (pairDb*2)
-        {
-            //3
-            case 6:
-                height = 3;
-                width = 2;
-                break;
-            //6
-            case 12:
-                height = 4;
-                width = 3;
-                break;
-            //8
-            case 16:
-                height = 4;
-                width = 4;
-                break;
-            //10
-            case 20:
-                height = 5;
-                width = 4;
-                break;
-            //15
-            case 30:
-                height = 6;
-                width = 5;
-                break;
-            //20
-            case 40:
-                height = 8;
-                width = 5;
-                break;
-            //24
-            case 48:
-                height = 8;
-                width = 6;
-                break;
-            //27
-            case 54:
-                height = 9;
-                width = 6;
-                break;
-        }
-        mainCamera.fieldOfView = 60;
-        Debug.Log(Screen.width);
-        if (height%2==0)
-        {
-            y = -cardsize / 2 + gap / 2 + (height / 2 * cardsize) + (height / 4 * gap);
-        }
-        else
-        {
-            //y = (height / 2 * cardsize) - gap * height;
-            y = (height * (cardsize + gap))/2-(cardsize+gap)/2;
-        }
-        if (width%2==0)
-        {
+        /* switch (pairDb*2)
+         {
+             //3
+             case 6:
+                 height = 3;
+                 width = 2;
+                 break;
+             //6
+             case 12:
+                 height = 4;
+                 width = 3;
+                 break;
+             //8
+             case 16:
+                 height = 4;
+                 width = 4;
+                 break;
+             //10
+             case 20:
+                 height = 5;
+                 width = 4;
+                 break;
+             //15
+             case 30:
+                 height = 6;
+                 width = 5;
+                 break;
+             //20
+             case 40:
+                 height = 8;
+                 width = 5;
+                 break;
+             //24
+             case 48:
+                 height = 8;
+                 width = 6;
+                 break;
+             //27
+             case 54:
+                 height = 9;
+                 width = 6;
+                 break;
+         }
 
-            basex = cardsize/2-gap / 2 - (width / 2*cardsize)-(width/4*gap);
-        }
-        else
-        {
-            basex = -(width * (cardsize + gap)) / 2 + (cardsize + gap) / 2;
+         //mainCamera.fieldOfView = 77;
+         //mainCamera.aspect = Screen.width / Screen.height;
 
+         float new_screen = (float)Screen.width / (float)Screen.height;
+         float size = mainCamera.orthographicSize;
+         mainCamera.orthographicSize = size * (new_screen / mainCamera.aspect);
+
+         if (height%2==0)
+         {
+             y = -cardsize / 2 + gap / 2 + (height / 2 * cardsize) + (height / 4 * gap);
+         }
+         else
+         {
+             //y = (height / 2 * cardsize) - gap * height;
+             y = (height * (cardsize + gap))/2-(cardsize+gap)/2;
+         }
+         if (width%2==0)
+         {
+
+             basex = cardsize/2-gap / 2 - (width / 2*cardsize)-(width/4*gap);
+         }
+         else
+         {
+             basex = -(width * (cardsize + gap)) / 2 + (cardsize + gap) / 2;
+
+         }*/
+
+
+        for (int i = 0; i < pairDb * 2; i++)
+        {
+            AddCardToBoard();
         }
-        
-        for (int i = 0; i < height; i++)
+
+        /*for (int i = 0; i < height; i++)
         {
             x = basex;
             for (int j = 0; j < width; j++)
             {
+                
                 AddCardToBoard();
                 x += cardsize+gap;
             }
             y -= cardsize+gap;
-        }      
+        }  */    
     }
 
     private void AddCardToBoard()
     {
         
         GameObject newCard = Instantiate(card);
+       
         
         newCard.transform.position = new Vector3(x, y, 0);
         int idx = GetSpriteIdx();
@@ -113,6 +129,8 @@ public class BoardSetup : MonoBehaviour
 
         newCard.GetComponent<CardManager>().id = cardidx;
         cardidx++;
+
+        newCard.transform.parent = board.transform;
         
         GetComponent<PairChacker>().cards.Add(newCard);
     }
@@ -128,6 +146,8 @@ public class BoardSetup : MonoBehaviour
 
     private void Awake()
     {
+        
+
         GameObject tmpcard = Instantiate(card);
         List<Sprite> availableSprites = new List<Sprite>();
         foreach (var item in tmpcard.GetComponent<CardSetup>().availableSprites)
@@ -136,7 +156,8 @@ public class BoardSetup : MonoBehaviour
         }
         tmpcard.SetActive(false);
         Debug.Log(availableSprites.Count);
-        
+
+       
         indexes = new List<int>();
         for (int i = 0; i < pairDb; i++)
         {
@@ -144,7 +165,8 @@ public class BoardSetup : MonoBehaviour
             do
             {
                 r = rng.Next(0, availableSprites.Count);
-            } while (indexes.Contains(r));           
+            } while (indexes.Contains(r));  
+            
             for (int j = 0; j < 2; j++)
             {
                 indexes.Add(r);
